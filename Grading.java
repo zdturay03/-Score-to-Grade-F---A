caa9255a72cbee8a3bc999068c4d7528c7995e3b
zdturay03 <turayzandradanes@gmail.com>
2024-07-02 08:22:18 +0000

    done adding output

---
+++ Grading.java
@@ -0,0 +1,23 @@
+importport java.util.Scanner;
+
+public class StudentGrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the student's score: ");
+        int score = scanner.nextInt();
+        
+        char grade = 'F'; // Default grade
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        }
+
+        System.out.println("The student's grade is: " + grade);
+    }
+}
